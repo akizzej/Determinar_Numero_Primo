ed98d4b5cbfc89ae8ba7dcff0e3ea2e1b297b261
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 19:05:25 -0300

    la funcion funcionaaaaaagit add .

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ Tips:
 Utilizar el operador modulo (%) para determinar si un número es divisor de otro.*/
 function numeroPrimo(num){
     // numeros primos 2 3 5 7 11 13 17 19 23 29
+    let primo = true;
     for (let i=2; i< num; i++){
         if (num % i ===0)
-        //retornaria false
-        return false;
+        primo = false;
+        //retornaria true
+        
+    }
+    if(primo===true){
+    console.log("Es primo!!!")
+    }else {
+        console.log("no es primo!!!")
+
     }
 }
-console.log(numeroPrimo(3));
+numeroPrimo(409);// llamo la funcion y le paso un numero para 5 debe imprimir que es primo
