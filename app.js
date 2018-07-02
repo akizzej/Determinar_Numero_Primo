3044aab638c5fa4e6f32161efb7fcc1e804d70ee
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 18:25:57 -0300

    se crean estructuras base para ejercicio

---
+++ app.js
@@ -0,0 +1,5 @@
+/*Crea una función que determine si un número es primo. En matemáticas, un número primo es un número natural mayor que 1 que tiene únicamente dos divisores distintos: él mismo y el 1.
+
+Tips:
+
+Utilizar el operador modulo (%) para determinar si un número es divisor de otro.*/app.js
