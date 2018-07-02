60dbd2e488ae81590f25acdf25258ff28f386163
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 18:35:00 -0300

    primer intento x determinar numero primo da error

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ Tips:
 
 Utilizar el operador modulo (%) para determinar si un número es divisor de otro.*/
 function numeroPrimo(num){
-    
+    // numeros primos 2 3 5 7 11 13 17 19 23 29
+    for (let i=2; i< num; i++){
+        if (num % i ===0)
+        //retornaria false
+        return false;
+    }
 }
+console.log(numeroPrimo(3));
